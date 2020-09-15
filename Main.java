df2af636f2b6068aa67b632f31d10584e23e39c7
blisn8405 <blisn8405@wrdsb.ca>
2020-09-15 21:11:36 +0000

    finsihed question 5

--- Main.java
+++ Main.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 /**
  *
  * @author 
@@ -9,7 +10,61 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
-  }
-}
+   // create a scanner for user input
+Scanner input = new Scanner(System.in);
+
+//integer if cannot be called
+int integer = 0;
+   //define a variable for the integer 
+int number1 = 18;
+   //define a variable for the  
+int number2 = 15;
+ //define a variable for the integer 
+ int number3 = 7;
+
+ if(number1 %3 == 0 && number1 %5 == 0){
+  System.out.println("Fizzbuzz");
+ } 
+ else {
+ if(number1 %3 == 0){
+   System.out.println("Fizz");
+ }
+ else {
+if(number1 %5 == 0)
+   System.out.println("Buzz");
+else 
+   System.out.println("18");
+      }       
+      }     
+ 
+ if(number2 %3 == 0 && number2 %5 == 0){
+  System.out.println("Fizzbuzz");
+ } 
+ else {
+ if(number2 %3 == 0)
+   System.out.println("Fizz");
+ else {
+if(number2 %5 == 0)
+   System.out.println("Buzz");
+else 
+   System.out.println("15");
+ }       
+ }    
+  if(number3 %3 == 0 && number3 %5 == 0){
+  System.out.println("Fizzbuzz");
+ } 
+ else {
+ if(number3 %3 == 0)
+   System.out.println("Fizz");
+ else {
+if(number3 %5 == 0)
+   System.out.println("Buzz");
+else 
+   System.out.println("7");
+      }       
+      }    
+  }  
+ }
+  
+
+ 
